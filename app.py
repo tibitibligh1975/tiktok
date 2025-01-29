9c49ae7bbb229c21dda3da11ceb8af85adb50e9e
tibitibligh1975 <tibitibligh1975@gmail.com>
2025-01-29 20:47:25 -0300

    Switch to Gunicorn for production deployment

--- app.py
+++ app.py
@@ -50,5 +50,4 @@ def gerar_grafico():
     return plot_url
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False) 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080))) 
