8825ddc87c1dcddf4ca0b9b41231c42d9e4f0ffa
tibitibligh1975 <tibitibligh1975@gmail.com>
2025-01-29 20:13:33 -0300

    Update default port to 8080

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def gerar_grafico():
     return plot_url
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     socketio.run(app, host='0.0.0.0', port=port, debug=False) 
